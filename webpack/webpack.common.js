9ab547906102afecbeff92d90ee73653cb6331e7
minkyung00 <mksin00@naver.com>
2022-12-29 22:13:46 +0900

    [CHORE] Add webpack common module options (#2)

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -11,6 +11,31 @@ const commonConfig = {
     assetModuleFilename: "assets/[name].[contenthash][ext][query]",
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  corejs: { version: 3, proposals: true },
+                  useBuiltIns: "usage",
+                  shippedProposals: true,
+                },
+              ],
+              "@babel/preset-typescript",
+              "@babel/preset-react",
+            ],
+          },
+        },
+        exclude: /(node_modules)/,
+      },
+    ],
+  },
   resolve: {
     modules: ["node_modules"],
     extensions: [".js", ".ts", ".tsx"],
